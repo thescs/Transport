1cdf717d1de05542c932228e6cfe1c1eaa2ed147
Vladimir Pruhyn <gthescs@gmail.com>
2021-11-12 10:45:55 +0200

    added distance

--- transport.class.php
+++ transport.class.php
@@ -27,16 +27,60 @@
 			return json_decode($this->removeBOM($this->tram), $object);
 		}
 		
-		public function getTransport($route = null, $type = "trol", $active = false, $bortnum = null){
+		// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula?page=1&tab=votes#tab-top
+		private	function distance($lat1, $lon1, $lat2, $lon2) {
+			$pi80 = M_PI / 180;
+			$lat1 *= $pi80;
+			$lon1 *= $pi80;
+			$lat2 *= $pi80;
+			$lon2 *= $pi80;
+		
+			$r = 6372.797; // mean radius of Earth in km
+			$dlat = $lat2 - $lat1;
+			$dlon = $lon2 - $lon1;
+			$a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlon / 2) * sin($dlon / 2);
+			$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+			$km = $r * $c;
+			
+			return $km;
+		}
+		
+		public function getTransport($route = null, $coordinates = null, $type = "trol", $active = false, $bortnum = null){
 			if(empty($this->$type)) return "Error: type '$type' passed is empty";
 			$data = json_decode($this->removeBOM($this->$type));
 			if(($route != null) || ($active) || ($bortnum != null))
 			{
+				$output = [];
 				foreach ($data->positions as $line){
-					if(!empty($route) && $route == $line->number) print_r($line);
+					//if(!empty($route) && $route == $line->number) print_r($line);
+					if(!empty($route) && $route == $line->number)
+					{
+						if(!empty($coordinates))
+						{
+							$diff = $this->distance($coordinates['lat'], $coordinates['lon'], $line->lat, $line->lng);
+							//var_dump($diff);
+							$line->difference = $diff;
+						}
+						array_push($output, $line);
+					}
 				}
+				return (object)$output;
 			}
+			else
+			{		
+				if (!empty($coordinates))
+				{
+					$output = [];
+					foreach ($data->positions as $record)
+					{
+							$diff = $this->distance($coordinates['lat'], $coordinates['lon'], $record->lat, $record->lng);
+							$record->difference = $diff;
+							array_push($output, $record);
+					}
+					return (object)$output;
+				}
 				else
-					var_dump($data);
+				return $data;
 		}
 	}
+}
